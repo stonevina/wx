ff22f259047458a46f03aa8861b7a03d073769bd
woshiwanting <smartwanting@gmail.com>
2016-12-02 21:47:45 +0800

    fix style

--- client/src/js/view/exam_view.js
+++ client/src/js/view/exam_view.js
@@ -31,7 +31,7 @@ var examView = Backbone.View.extend({
     this.examModel = new ExamModel;
     this.questionIndex = 0;
     //答题数量
-    this.limit = 20;
+    this.limit = 10;
 
     this.scoreView = new ScoreView();
 
